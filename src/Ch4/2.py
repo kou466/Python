8e99f1c1545531011c9a5c9c328100bf08a2ffa2
kou466 <1226hajing@gmail.com>
2019-04-11 20:01:47 +0900

    Chapter4

---
+++ src/Ch4/2.py
@@ -0,0 +1,6 @@
+a = int(input("숫자1: "))
+b = int(input("숫자2: "))
+
+print(a, "/", b, "=", (a/b))
+print(a, "//", b, "=", (a//b))
+print(a, "%", b, "=", (a%b))
---
+++ src/Ch4/3.py
@@ -0,0 +1,4 @@
+import math
+
+radius = int(input("반지름: "))
+print("원의 넓이:", radius*radius*math.pi)
