f91cca992390e0458af4ff0ff00af9712add8256
kou466 <1226hajing@gmail.com>
2019-05-28 21:02:21 +0900

    Chapter9

--- src/Ch9/05.py
+++ src/Ch9/05.py
@@ -3,8 +3,8 @@ num2 = int(input("정수2: "))
 
 
 def div_qr(n1, n2):
-    return n1 // n2, n1 % n2
+    return (n1 // n2, n1 % n2)
 
 
-quotient, remainder = div_qr(num1, num2)
+(quotient, remainder) = div_qr(num1, num2)
 print("몫: {0}, 나머지: {1}".format(quotient, remainder))
